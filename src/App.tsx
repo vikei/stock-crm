471e333fe5ea99a43103f1c2de60c2d6475f71f1
vikei <vikei@gmail.com>
2020-10-16 15:17:57 +0300

    Add apollo & generate type graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import {BrowserRouter, Route} from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <GlobalStyles />
       <BrowserRouter>
         <Route path="/">Ready to start...</Route>
       </BrowserRouter>
-    </>
+    </ApolloProvider>
   );
 }
 
